b1b63089a0de45bbb663b3895cb1d5920fc35644
yasen osama <yaseno933@gmail.com>
2026-02-17 18:10:08 +0200

    dbcontext

---
+++ rev-back.Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rev_back.Domain.Entities;
+
+namespace rev_back.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
